46528f9fe9468572837721bb9817be0191f966ab
DevCoM1995 <devcomca@gmail.com>
2015-07-12 18:25:59 -0400

    Integrating code
    Added in menu code, also changed over spin code to new version

---
+++ Assets/Scripts/MenuReturn.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuReturn : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetMouseButtonUp(0)){
+				RaycastHit targetFind;
+				Ray targetSeek = Camera.main.ScreenPointToRay (Input.mousePosition);
+				if (Physics.Raycast (targetSeek, out targetFind)) {
+					//gets stats of clicked building, triggers the GUI popups
+					if (targetFind.collider.gameObject.tag == "Button") {
+					Application.LoadLevel ("MenuTest");
+					}
+	}
+}
+	}
+}
---
+++ Assets/Scripts/MenuScaleEffect.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuScaleEffect : MonoBehaviour {
+	public GameObject parent;
+	public float rotOffset = 0.0f;
+	float scaleFactor = 0.0f;
+	public float rotationSet;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//rotOffset keeps the scale accurate for all icons past the first
+		rotationSet = (parent.transform.eulerAngles.z + rotOffset) % 360;
+		if(rotationSet >= 180.0f){
+			//Shrinks object while it moves up and to the left
+			scaleFactor = (rotationSet) / 360.0f;
+		}else{
+			//Grows object when it hits the halfway point when going left
+			scaleFactor = 1.0f - ((rotationSet)/ 360.0f);
+		}
+		transform.localScale = new Vector3 (scaleFactor, scaleFactor, 0);
+		//Keeps all icons facing down
+		transform.localEulerAngles = new Vector3(0,0,(360 - parent.transform.eulerAngles.z));
+	}
+}
