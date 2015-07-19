aeaf9dd0f12eca828523eab6b97ec2acc10121c7
DevCoM1995 <devcomca@gmail.com>
2015-07-19 13:45:12 -0400

    Revert "More menu stuff, started world select"
    This reverts commit 6109dcda90d6fd1693574c91a8d7402beb1224b8.

--- Assets/Scripts/MenuScaleEffect.cs
+++ Assets/Scripts/MenuScaleEffect.cs
@@ -5,11 +5,8 @@ public class MenuScaleEffect : MonoBehaviour {
 	public GameObject parent;
 	public float rotOffset = 0.0f;
 	float scaleFactor = 0.0f;
-	float opFactor = 0.0f;
-	public float secondaryOp = 0.0f;
 	public float rotationSet;
-	public float opAdjust = 2.0f;
-	Color transparency = new Color(1,1,1,1);
+
 	// Use this for initialization
 	void Start () {
 	
@@ -22,23 +19,12 @@ public class MenuScaleEffect : MonoBehaviour {
 		if(rotationSet >= 180.0f){
 			//Shrinks object while it moves up and to the left
 			scaleFactor = (rotationSet) / 360.0f;
-			opFactor = (rotationSet) / 360.0f;
-			secondaryOp = 1.0f - ((rotationSet)/ 360.0f);
-
 		}else{
 			//Grows object when it hits the halfway point when going left
 			scaleFactor = 1.0f - ((rotationSet)/ 360.0f);
-			opFactor = 1.0f - ((rotationSet)/ 360.0f);
-			secondaryOp = (rotationSet) / 360.0f;
 		}
-
-		scaleFactor -= 0.3f;
 		transform.localScale = new Vector3 (scaleFactor, scaleFactor, 0);
 		//Keeps all icons facing down
 		transform.localEulerAngles = new Vector3(0,0,(360 - parent.transform.eulerAngles.z));
-		transparency = GetComponent<SpriteRenderer> ().material.color;
-
-		transparency.a = opFactor - (secondaryOp *opAdjust);
-		GetComponent<SpriteRenderer> ().material.color = transparency;
 	}
 }
--- Assets/Scripts/MenuSpinScript.cs
+++ Assets/Scripts/MenuSpinScript.cs
@@ -4,18 +4,19 @@ using System.Collections;
 public class MenuSpinScript : MonoBehaviour {
 	//Increases spin speed
 	public float multiplier = 10f;
-	public GameObject Child;
+	public GameObject CamLock1;
+	public GameObject CamLock2;
+	public GameObject CamLock3;
 	//Can only spin if this is true;
 	bool spinner = false;
-	public bool spinLock = false;
+	bool spinLock = false;
 	//Implemented early to allow the player to stop over a button and not accidentally trigger it
 	float clickTime = 0;
 	//Centralized variable for how long the player can hold before it becomes a drag instead of a press
 	float clickDelay = 0.1f;
 	float rotX = 0.0f;
 	float rotY = 0.0f;
-	public int lockThreshold = 72;
-	public int menuPosition = 0;
+	int menuPosition = 0;
 	// Use this for initialization
 	void Start () {
 		
@@ -34,15 +35,28 @@ public class MenuSpinScript : MonoBehaviour {
 			if(clickTime > clickDelay){
 				//Locks position to nearest interval of 60
 				float rotation = transform.eulerAngles.z;
-				float lockRot = Mathf.Round(rotation /lockThreshold)*lockThreshold;
+				float lockRot = Mathf.Round(rotation /72)*72;
 
-				menuPosition = (int) lockRot/lockThreshold;
-				if(Child){
-				Child.GetComponent<MenuClickScript>().menuPosition = menuPosition;
-				}
+				menuPosition = (int) lockRot/72;
 				transform.rotation = Quaternion.Euler(0, 0, lockRot);
 
-			}
+			}else{
+				RaycastHit targetFind;
+				Ray targetSeek = Camera.main.ScreenPointToRay (Input.mousePosition);
+					if (Physics.Raycast (targetSeek, out targetFind)) {
+							//gets stats of clicked building, triggers the GUI popups
+							if (targetFind.collider.gameObject.tag == "Button") {
+						//what triggers changes based on what menu the camera is focused on.
+						if(targetFind.transform.position.x == 0.0f){
+							ButtonClick();
+						}else if(targetFind.transform.position.x < 0.0f){
+							ReturnMain(0);
+						}else{
+							ReturnMain(1);
+						}
+							}
+						}
+					}
 			//resets time
 			clickTime = 0;
 		}
@@ -69,5 +83,47 @@ public class MenuSpinScript : MonoBehaviour {
 			}
 		}
 	}
+	void ButtonClick(){
+		//Location of the menu dial determines the result
+		//Commenting out LoadLevel calls until we have levels to load
+		switch (menuPosition) {
+		case 0:
+			//Loads the scene used for playing the game
+			//Application.LoadLevel ("TestScene");
+			break;
+		case 1:
+			//Simple menu like this might as well exist within the scene to save on load times
+			Camera.main.transform.position = CamLock2.transform.position;
+			transform.localEulerAngles = new Vector3(0,0,0);
+			spinLock = true;
+			Debug.Log ("Settings");
+			break;
+		case 2:
+			//Same as the settings menu
+			Camera.main.transform.position = CamLock3.transform.position;
+			transform.localEulerAngles = new Vector3(0,0,0);
+			spinLock = true;
+			Debug.Log ("Limiters");
+			break;
+		case 3:
+			//This and 4 load more levels
+			//Application.LoadLevel ("AltSceneTest");
+			break;
+		case 4:
+			//Application.LoadLevel ("AltSceneTest");
+			break;
+		case 5:
+			//The way menuPosition is assigned results in it being 5 if the dial is released at a certain point
+			//Application.LoadLevel ("TestScene");
+			break;
+		default:
+			break;
+		}
+	}
 
+	void ReturnMain(int currentMenu){
+		//Later on currentMenu will be used to only save stuff that might have been changed in that menu
+		spinLock = false;
+		Camera.main.transform.position = CamLock1.transform.position;
+	}
 }
