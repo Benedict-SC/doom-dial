40be36013b1dbaf4f9888eafddf2c48a2e9e0dd3
DevCoM1995 <devcomca@gmail.com>
2015-07-19 13:45:49 -0400

    Revert "Revert "More menu stuff, started world select""
    This reverts commit aeaf9dd0f12eca828523eab6b97ec2acc10121c7.

--- Assets/Scripts/MenuSelectScaleEffect.cs
+++ Assets/Scripts/MenuSelectScaleEffect.cs
@@ -1,44 +1,54 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class MenuScaleEffect : MonoBehaviour {
+public class MenuSelectScaleEffect : MonoBehaviour {
 	public GameObject parent;
 	public float rotOffset = 0.0f;
 	float scaleFactor = 0.0f;
-	float opFactor = 0.0f;
+	public float opFactor = 0.0f;
 	public float secondaryOp = 0.0f;
 	public float rotationSet;
 	public float opAdjust = 2.0f;
 	Color transparency = new Color(1,1,1,1);
+	float temp = 360.0f;
 	// Use this for initialization
 	void Start () {
-	
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
 		//rotOffset keeps the scale accurate for all icons past the first
 		rotationSet = (parent.transform.eulerAngles.z + rotOffset) % 360;
-		if(rotationSet >= 180.0f){
+		if(rotationSet <= 60.0f){
+			temp = 120.0f;
+			opAdjust = 2.0f;
 			//Shrinks object while it moves up and to the left
-			scaleFactor = (rotationSet) / 360.0f;
-			opFactor = (rotationSet) / 360.0f;
-			secondaryOp = 1.0f - ((rotationSet)/ 360.0f);
-
-		}else{
+			scaleFactor = 1.0f - ((rotationSet)/ temp);
+			opFactor = (1.0f - ((rotationSet)/ temp));
+			secondaryOp = (rotationSet) / temp;
+			scaleFactor *= scaleFactor;
+			
+		}else if(rotationSet >= 270.0f){
+			temp = 360.0f;
+			opAdjust = 3.0f;
 			//Grows object when it hits the halfway point when going left
-			scaleFactor = 1.0f - ((rotationSet)/ 360.0f);
-			opFactor = 1.0f - ((rotationSet)/ 360.0f);
-			secondaryOp = (rotationSet) / 360.0f;
-		}
+			scaleFactor = (rotationSet) / temp;
+			opFactor = (rotationSet) / temp;
+			secondaryOp = 1.0f - ((rotationSet)/ temp);
 
-		scaleFactor -= 0.3f;
+			scaleFactor *= scaleFactor;
+		}
+		
+
 		transform.localScale = new Vector3 (scaleFactor, scaleFactor, 0);
 		//Keeps all icons facing down
 		transform.localEulerAngles = new Vector3(0,0,(360 - parent.transform.eulerAngles.z));
 		transparency = GetComponent<SpriteRenderer> ().material.color;
-
-		transparency.a = opFactor - (secondaryOp *opAdjust);
+		
+		transparency.a = opFactor - (secondaryOp * opAdjust);
 		GetComponent<SpriteRenderer> ().material.color = transparency;
 	}
 }
+
