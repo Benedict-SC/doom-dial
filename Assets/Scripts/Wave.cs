e34192f55f20775cf58012b39f31398e7e83f70f
Duncan Holter <djholter@ncsu.edu>
2015-09-06 14:16:55 -0400

    waves have random spawntimes now

--- Assets/Scripts/Wave.cs
+++ Assets/Scripts/Wave.cs
@@ -8,45 +8,114 @@ public class Wave{
 
 	int levelID;
 	int waveID;
+	int maxTime;
+	int interval;
 	List<GameObject> enemies;
 
 	public Wave (Dictionary<string,System.Object> json){
+	
+		System.Random rand = new System.Random();
+	
 		enemies = new List<GameObject> ();
 		//note: ints in MiniJSON come out as longs, so must be cast twice
 		levelID = (int)(long)json ["levelID"];
 		waveID = (int)(long)json ["waveID"];
+		maxTime = (int)(long)json ["maxMilliseconds"];
+		interval = (int)(long)json ["minimumInterval"];
 		List<System.Object> enemyjson = (List<System.Object>)json ["enemies"];
-		foreach (System.Object enemy in enemyjson) {
+		
+		//step one: create a randomized list of spawn times
+		int slots = maxTime / interval;
+		int occupants = enemyjson.Count;
+		
+			//create bool array to randomize
+		List<bool> timeslots = new List<bool>();
+		for(int i = 0; i < slots; i++){
+			if(i < occupants){
+				timeslots.Add(true);
+			}else {
+				timeslots.Add(false);
+			}
+		}
+			//randomize this array (fisher-yates shuffle)
+		for(int i = slots - 1; i > 0; i--){
+			int j = rand.Next(i+1);
+			bool temp = timeslots[i];
+			timeslots[i] = timeslots[j];
+			timeslots[j] = temp;
+		}
+			//create corresponding array of random-ish long bonuses to positions
+		List<long> timeChaos = new List<long>();
+		for(int i = 0; i < timeslots.Count; i++){
+			timeChaos.Add(0);
+		}
+		for(int i = 0; i < timeChaos.Count; i++){
+			if(timeslots[i]){ //if an enemy should spawn in this timeframe,
+				//get the previous bonus to make sure you're the min distance away
+				long previous = 0;
+				if(i > 0){
+					previous = timeChaos[i-1];
+				}
+				//create random spawn time within the time slot allotted
+				long chaos = rand.Next((int)previous,(int)interval);
+				timeChaos[i] = chaos;
+			}
+		}
+			//check to make sure nothing went wrong and timeslots and timechaos are both of length slots
+		if(timeslots.Count != slots){
+			Debug.Log("timeslots is wrong length! slots is " + slots + " but timeslots length is " + timeslots.Count);
+		}
+		if(timeChaos.Count != slots){
+			Debug.Log("timechaos is wrong length! slots is " + slots + " but timechaos length is " + timeChaos.Count);
+		}
+		
+			//finally, create final list of spawn times
+		List<long> spawntimesInMillis = new List<long>();
+		for(int i = 0; i < timeslots.Count; i++){
+			if(timeslots[i]){
+				long spawntime = i * interval;
+				spawntime += timeChaos[i];
+				spawntimesInMillis.Add(spawntime);
+			}
+		}
+		//check to make sure nothing went wrong and spawntimes is of length occupants
+		if(spawntimesInMillis.Count != enemyjson.Count){
+			Debug.Log ("spawntimes and enemies don't match! (" + spawntimesInMillis.Count + "/" + enemyjson.Count + ")"); 
+		}
+		
+		for(int i = 0; i < enemyjson.Count; i++){
+			System.Object enemy = enemyjson[i];
 			Dictionary<string,System.Object> enemydict = (Dictionary<string,System.Object>)enemy;
 			//would load from bestiary using (string)enemydict["enemyID"], but no bestiary yet
-			long spawntimeInMillis = (long)enemydict["spawntime"];
+			//long spawntimeInMillis = (long)enemydict["spawntime"];
 			int track = (int)(long)enemydict["trackID"];
 			int trackpos = (int)(long)enemydict["trackpos"];
 			//make enemy
 			GameObject enemyspawn = GameObject.Instantiate (Resources.Load ("Prefabs/Enemy")) as GameObject;
 			enemyspawn.SetActive(false);
 			EnemyController ec = enemyspawn.GetComponent<EnemyController>();
-
+			
 			//give enemy a filename to load from
 			string filename = (string) enemydict["enemyID"];
 			ec.SetSrcFileName(filename);
 			ec.SetTrackID(track);
 			ec.SetTrackLane(trackpos);
-
+			
 			//calculate and set position
 			float degrees = (track-1)*60; //clockwise of y-axis
 			degrees += 15*trackpos; //negative trackpos is left side, positive is right side, 0 is middle
 			degrees = ((360-degrees) + 90)%360; //convert to counterclockwise of x axis
 			degrees *= Mathf.Deg2Rad;
-
+			
 			enemyspawn.transform.position = new Vector3(radius*Mathf.Cos(degrees),radius*Mathf.Sin(degrees),0);
-
+			
 			//set spawn time
-			ec.SetSpawnTime(spawntimeInMillis);
+			ec.SetSpawnTime(spawntimesInMillis[i]);
 			enemies.Add(enemyspawn);
-
-			//ec.ConfigureEnemy ();
 		}
+		/*foreach (System.Object enemy in enemyjson) {
+			//ec.ConfigureEnemy ();
+		}*/
 	}
 
 	public List<GameObject> GetEnemies(){
