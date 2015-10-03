f1c8d39c528b6dee323190b02429675e9946cbbb
DevCoM1995 <devcomca@gmail.com>
2015-10-02 20:48:24 -0400

    More work on game menu
    Fixed rotations, tweaked dial spin to not start if you start the touch
    on a button.

--- Assets/Scripts/SpinScript.cs
+++ Assets/Scripts/SpinScript.cs
@@ -17,7 +17,9 @@ public class SpinScript : MonoBehaviour, EventHandler {
 	
 	float originalRot = 0.0f; //the angle of the mouse when you start the spin
 	float origz = 0.0f; //the angle of the dial when you start the spin
-	
+
+	bool buttonCheck = false; //stops spinning if touch starts on a button
+
 	// Use this for initialization
 	void Start () {
 		EventManager em = EventManager.Instance ();
@@ -28,21 +30,35 @@ public class SpinScript : MonoBehaviour, EventHandler {
 	public void HandleEvent(GameEvent ge){
 		Vector3 mousepos = InputWatcher.GetInputPosition ();
 		if (ge.type.Equals ("mouse_release")) {
-
+			if(!buttonCheck){
 			//Stops the dial from spinning more
-			spinner = false;
+			
 			//Only tries to lock if the spinner has a chance of moving
-			if(clickTime > clickDelay){
+			if (clickTime > clickDelay) {
 				//Locks position to nearest interval of 60
 				float rotation = transform.eulerAngles.z;
-				float lockRot = Mathf.Round(rotation /60)*60;
-				transform.rotation = Quaternion.Euler(0, 0, lockRot);
+				float lockRot = Mathf.Round (rotation / 60) * 60;
+				transform.rotation = Quaternion.Euler (0, 0, lockRot);
+			}
 			}
 			//resets time
 			clickTime = 0;
 			touchDown = false;
-		}else if(ge.type.Equals("mouse_click")){
+			spinner = false;
+			buttonCheck = false;
+		} else if (ge.type.Equals ("mouse_click")) {
 			//Allows the dial to start spinning
+			RaycastHit targetFind;
+
+			Ray targetSeek = Camera.main.ScreenPointToRay (InputWatcher.GetTouchPosition ());
+			if (Physics.Raycast (targetSeek, out targetFind)) {
+
+				//sees if ray collided with the start button
+				if (targetFind.collider.gameObject.tag == "Button") {
+
+					buttonCheck = true;
+				}
+			}
 			if(spinner == false){
 				originalRot = Mathf.Atan2(mousepos.y,mousepos.x);
 				origz = transform.eulerAngles.z;
@@ -57,21 +73,22 @@ public class SpinScript : MonoBehaviour, EventHandler {
 	void Update () {
 		Vector3 mousepos = InputWatcher.GetInputPosition ();
 		//Debug.Log (touchDown);
-		if(touchDown){
+		if (touchDown) {
+			if (!buttonCheck) {
+				//Debug.Log ("mouse down");
+				clickTime += Time.deltaTime;
+				//Only allows the dial to spin if the player has been pressing for over a certain amount of time
+				if (spinner && clickTime > clickDelay) {
 
-			//Debug.Log ("mouse down");
-			clickTime += Time.deltaTime;
-			//Only allows the dial to spin if the player has been pressing for over a certain amount of time
-			if(spinner && clickTime > clickDelay){
+					//Probably not the best for dealing with movement on both axis, 
+					//also will change code to touch controls once we start testing the game on mobile
+					float angle = Mathf.Atan2 (mousepos.y - transform.position.y, mousepos.x - transform.position.x);// (mousepos.y,mousepos.x);
+					float degrees = (Mathf.Rad2Deg * angle);
+					float origDegrees = Mathf.Rad2Deg * originalRot;
 
-				//Probably not the best for dealing with movement on both axis, 
-				//also will change code to touch controls once we start testing the game on mobile
-				float angle = Mathf.Atan2(mousepos.y - transform.position.y,mousepos.x - transform.position.x);// (mousepos.y,mousepos.x);
-				float degrees = (Mathf.Rad2Deg * angle);
-				float origDegrees = Mathf.Rad2Deg * originalRot;
-
-				transform.rotation = Quaternion.Euler(0,0,(origz + (degrees - origDegrees)*rotScale)%360);
-				//transform.Rotate(0, 0, Input.GetAxis("Mouse Y") + Input.GetAxis("Mouse X")* multiplier, Space.World);
+					transform.rotation = Quaternion.Euler (0, 0, (origz + (degrees - origDegrees) * rotScale) % 360);
+					//transform.Rotate(0, 0, Input.GetAxis("Mouse Y") + Input.GetAxis("Mouse X")* multiplier, Space.World);
+				}
 			}
 		}
 	}
