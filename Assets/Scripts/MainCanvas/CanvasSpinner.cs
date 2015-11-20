b5c67b2709948cfd3f48849bb2998e73207f06c4
Duncan Holter <djholter@ncsu.edu>
2015-11-19 22:53:49 -0500

    dial spins

--- Assets/Scripts/MainCanvas/CanvasSpinner.cs
+++ Assets/Scripts/MainCanvas/CanvasSpinner.cs
@@ -9,32 +9,52 @@ public class CanvasSpinner : MonoBehaviour,EventHandler{
 	Canvas canvas;
 	
 	bool spinning = false;
-	float startingRot;
+	float startingMouseRot;
+	float startingDialRot;
 	GunButton[] gunButtons = new GunButton[6];
 	
 	public void Start(){
 		canvas = GameObject.Find("Canvas").GetComponent<Canvas>();
+		RectTransform rt = (RectTransform)transform;
 		EventManager em = EventManager.Instance ();
 		em.RegisterForEventType ("mouse_release", this);
 		em.RegisterForEventType ("mouse_click", this);
+		anchorX = rt.anchoredPosition.x;
+		anchorY = rt.anchoredPosition.y;
 		
 		for(int i = 1; i < 7; i++){
 			gunButtons[i-1] = GameObject.Find("Button"+i).GetComponent<GunButton>();
 		}
 	}
 	public void HandleEvent(GameEvent ge){
+		Vector3 mousepos = InputWatcher.GetCanvasInputPosition((RectTransform)canvas.transform);
 		if(ge.type.Equals("mouse_click")){
 			if(TouchIsOnGunButtons()){
-				Debug.Log("you touched a gun button");
+				return;
 			}else{
-				Debug.Log ("you touched not a gun button");
+				spinning = true;
+				startingDialRot = transform.eulerAngles.z * Mathf.Deg2Rad;
+				startingMouseRot = Mathf.Atan2(mousepos.y-anchorY,mousepos.x-anchorX);
 			}
 		}else if(ge.type.Equals("mouse_release")){
-		
+			if(TouchIsOnGunButtons())
+				return;
+			spinning = false;
+			float mouseAngle = Mathf.Atan2 ((mousepos.y - anchorY) - transform.position.y, (mousepos.x-anchorX) - transform.position.x);
+			float angleChange = mouseAngle-startingMouseRot;
+			transform.eulerAngles = new Vector3(transform.eulerAngles.x,transform.eulerAngles.y,(startingDialRot + angleChange)*Mathf.Rad2Deg);
+			float rotation = transform.eulerAngles.z;
+			float lockRot = Mathf.Round (rotation / 60) * 60;
+			transform.rotation = Quaternion.Euler (0, 0, lockRot);
 		}
 	}
 	public void Update(){
-		
+		if(!spinning)
+			return;
+		Vector3 mousepos = InputWatcher.GetCanvasInputPosition((RectTransform)canvas.transform);
+		float mouseAngle = Mathf.Atan2 ((mousepos.y - anchorY) - transform.position.y, (mousepos.x-anchorX) - transform.position.x);
+		float angleChange = mouseAngle-startingMouseRot;
+		transform.eulerAngles = new Vector3(transform.eulerAngles.x,transform.eulerAngles.y,(startingDialRot + angleChange)*Mathf.Rad2Deg);
 	}
 	public bool TouchIsOnGunButtons(){
 		foreach (GunButton gb in gunButtons){
