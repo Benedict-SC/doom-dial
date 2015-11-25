a1aace5aabe279aabc64413950057683ff4425fc
Duncan Holter <djholter@ncsu.edu>
2015-11-25 13:20:32 -0500

    set up the camera toggle with the menu button

---
+++ Assets/Scripts/MainCanvas/CameraToggle.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class CameraToggle : MonoBehaviour{
+	public void Start(){
+	
+	}
+	public void Update(){
+	
+	}
+	public void GoToMenu(){
+		transform.position = new Vector3(transform.position.x,transform.position.y,20f);
+	}
+	public void GoToGame(){
+		transform.position = new Vector3(transform.position.x,transform.position.y,-10f);
+	}
+}
