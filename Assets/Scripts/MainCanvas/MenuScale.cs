9454ff2c360ba0151e2f38343b4285d2e8290e6b
DevCoM1995 <devcomca@gmail.com>
2015-11-25 23:56:02 -0500

    More work on in game menu

---
+++ Assets/Scripts/MainCanvas/MenuScale.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuScale : MonoBehaviour {
+	public GameObject parent;
+	public float rotOffset = 0.0f;
+	float scaleFactor = 0.0f;
+	public float opFactor = 0.0f;
+	public float secondaryOp = 0.0f;
+	public float rotationSet;
+	public float opAdjust = 2.0f;
+	float transparency = 0.0f;
+	float temp = 360.0f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		//rotOffset keeps the scale accurate for all icons past the first
+		rotationSet = (parent.transform.eulerAngles.z + rotOffset) % 360;
+		if(rotationSet <= 60.0f){
+			temp = 120.0f;
+			opAdjust = 2.0f;
+			//Shrinks object while it moves up and to the left
+			scaleFactor = 1.0f - ((rotationSet)/ temp);
+			opFactor = (1.0f - ((rotationSet)/ temp));
+			secondaryOp = (rotationSet) / temp;
+			scaleFactor *= scaleFactor;
+			
+		}else if(rotationSet >= 270.0f){
+			temp = 360.0f;
+			opAdjust = 3.0f;
+			//Grows object when it hits the halfway point when going left
+			scaleFactor = (rotationSet) / temp;
+			opFactor = (rotationSet) / temp;
+			secondaryOp = 1.0f - ((rotationSet)/ temp);
+			
+			scaleFactor *= scaleFactor;
+		}
+		
+		
+		transform.localScale = new Vector3 (scaleFactor, scaleFactor, 0);
+		//Keeps all icons facing down
+		transform.localEulerAngles = new Vector3(0,0,(360 - parent.transform.eulerAngles.z));
+		//transparency = GetComponent<CanvasRenderer> ().GetAlpha;
+		
+		transparency = opFactor - (secondaryOp * opAdjust);
+		GetComponent<CanvasRenderer> ().SetAlpha (transparency);
+	}
+}
---
+++ Assets/Scripts/MainCanvas/MenuSpinner.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuSpinner : MonoBehaviour,EventHandler {
+	public float anchorX = 0;
+	public float anchorY = 0;
+	
+	Canvas canvas;
+	
+	bool spinning = false;
+	float startingMouseRot;
+	float startingDialRot;
+	// Use this for initialization
+	void Start () {
+		canvas = GameObject.Find("MenuCanvas").GetComponent<Canvas>();
+		RectTransform rt = (RectTransform)transform;
+		EventManager em = EventManager.Instance ();
+		em.RegisterForEventType ("mouse_release", this);
+		em.RegisterForEventType ("mouse_click", this);
+		anchorX = rt.anchoredPosition.x;
+		anchorY = rt.anchoredPosition.y;
+	}
+	public void HandleEvent(GameEvent ge){
+		if(!Pause.paused)
+			return;
+		Vector3 mousepos = InputWatcher.GetCanvasInputPosition((RectTransform)canvas.transform);
+		if(ge.type.Equals("mouse_click")){
+			if(spinning){
+				return;
+			}else{
+				spinning = true;
+				startingDialRot = transform.eulerAngles.z * Mathf.Deg2Rad;
+				startingMouseRot = Mathf.Atan2(mousepos.y-anchorY,mousepos.x-anchorX);
+			}
+		}else if(ge.type.Equals("mouse_release")){
+			if(!spinning)
+				return;
+			spinning = false;
+			float mouseAngle = Mathf.Atan2 ((mousepos.y - anchorY) - transform.position.y, (mousepos.x-anchorX) - transform.position.x);
+			float angleChange = mouseAngle-startingMouseRot;
+			transform.eulerAngles = new Vector3(transform.eulerAngles.x,transform.eulerAngles.y,(startingDialRot + angleChange)*Mathf.Rad2Deg);
+			float rotation = transform.eulerAngles.z;
+			float lockRot = Mathf.Round (rotation / 30) * 30;
+			transform.rotation = Quaternion.Euler (0, 0, lockRot);
+		}
+	}
+	// Update is called once per frame
+	void Update () {
+		if(!Pause.paused)
+			return;
+		if(!spinning)
+			return;
+		Vector3 mousepos = InputWatcher.GetCanvasInputPosition((RectTransform)canvas.transform);
+		float mouseAngle = Mathf.Atan2 ((mousepos.y - anchorY) - transform.position.y, (mousepos.x-anchorX) - transform.position.x);
+		float angleChange = mouseAngle-startingMouseRot;
+		transform.eulerAngles = new Vector3(transform.eulerAngles.x,transform.eulerAngles.y,(startingDialRot + angleChange)*Mathf.Rad2Deg);
+	}
+	public bool IsSpinning(){
+		return spinning;
+	}
+}
