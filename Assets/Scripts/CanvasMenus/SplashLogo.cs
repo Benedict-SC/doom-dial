a73dce6020f5bca05263d7bdc66d916622ea4233
Thom Cote <tcote000college@gmail.com>
2016-05-13 21:29:03 -0500

    Starting the splash screen/update check

---
+++ Assets/Scripts/CanvasMenus/SplashLogo.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class SplashLogo : MonoBehaviour {
+
+    Image logoImg;
+    Color originalColor;
+    Color finalColor;
+
+    public float fadeInSpeed = 1;
+
+	// Use this for initialization
+	void Start () {
+
+        logoImg = GetComponent<Image>();
+        logoImg.color = new Color(logoImg.color.r, logoImg.color.g, logoImg.color.b, 0f);
+
+        originalColor = logoImg.color;
+        finalColor = new Color(originalColor.r, originalColor.g, originalColor.b, 1f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Color newColor = Color.Lerp(originalColor, finalColor, Time.time / (1/fadeInSpeed));
+        //oldTime += Time.deltaTime * 50;
+        logoImg.color = newColor;
+	}
+}
