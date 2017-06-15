ee9b1b0be322ec8750cb1f0c33046d1f4ff2c453
Thom Cote <tcote000college@gmail.com>
2017-06-15 12:24:35 -0500

    finished up homing for radial bullets

--- Assets/Scripts/MainCanvas/BulletRadial.cs
+++ Assets/Scripts/MainCanvas/BulletRadial.cs
@@ -9,6 +9,9 @@ public class BulletRadial : Bullet {
     public GameObject ignoredEnemy; //enemy that tripped the ptrap - is ignored
     int initialLane;
 
+    public GameObject homingTarget;
+    float homingStrengthConstant = 6f;
+
     void Start()
     {
         base.Start();
@@ -18,16 +21,42 @@ public class BulletRadial : Bullet {
 	// Update is called once per frame
 	void Update ()
     {
-        Debug.Log("BulletRadial range is " + range);
+        //Debug.Log("BulletRadial range is " + range);
         //don't do anything if paused
         if (Pause.paused)
             return;
         //basic movement
-        rt.anchoredPosition = new Vector2(rt.anchoredPosition.x + vx, rt.anchoredPosition.y + vy);
+        if (attraction == 0)
+        {
+            rt.anchoredPosition = new Vector2(rt.anchoredPosition.x + vx, rt.anchoredPosition.y + vy);
+        }
+        else if (attraction > 0)
+        {
+            rt.anchoredPosition = new Vector2(rt.anchoredPosition.x + vx, rt.anchoredPosition.y + vy);
+            if (homingTarget == null)
+            {
+                SetHomingTarget();
+            }
+            else if (homingTarget != null)
+            {
+                float speedMult = speed / PieceParser.SPEED_CONSTANT;
+                Vector2 targPos = homingTarget.GetComponent<RectTransform>().anchoredPosition;
+                Vector2 homeDir = targPos - rt.anchoredPosition; //direction to home in
+                homeDir = homeDir.normalized * homingStrengthConstant * speedMult;
+                //Debug.Log("homeDir: " + homeDir.ToString());
+                homeDir *= attraction;
+                //Debug.Log ("vx: " + vx);
+                //Debug.Log ("vy: " + vy);
+                vx += homeDir.x;
+                vy += homeDir.y;
+                //Debug.Log ("new vx: " + vx);
+                //Debug.Log ("new vy: " + vy);
+            }
+        }
         //get distance traveled
         distance = (float)Math.Sqrt((rt.anchoredPosition.x - spawnx) * (rt.anchoredPosition.x - spawnx)
                                 + (rt.anchoredPosition.y - spawny) * (rt.anchoredPosition.y - spawny));
-        Debug.Log("BulletRadial distance is " + distance);
+        //Debug.Log("BulletRadial distance is " + distance);
         //die at range or (not apparently) at zone borders
         //dying when hitting the dial is handled in Dial.cs's OnTriggerEnter2D()
         if (distance > range/* || initialLane != GetCurrentLaneID()*/)
@@ -35,6 +64,40 @@ public class BulletRadial : Bullet {
             Debug.Log("calling Collide() on BulletRadial");
             Collide();
         }
+    }
+
+    //sets homing target
+    public void SetHomingTarget()
+    {
+        homingTarget = FindNearestEnemy();
+    }
+
+    //returns the nearest enemy in this bullet's zone, ignoring ignoredEnemy
+    public GameObject FindNearestEnemy()
+    {
+        float minDist = 9999f;
+        GameObject minEnemy = null;
+        GameObject[] enemiesOnScreen = GameObject.FindGameObjectsWithTag("Enemy");
+        foreach (GameObject enemy in enemiesOnScreen)
+        {
+            if (enemy != null && enemy != ignoredEnemy)
+            {
+                Enemy ec = enemy.GetComponent<Enemy>();
+                if (ec.GetCurrentTrackID() == GetCurrentLaneID()) //if this enemy is in this bullet's zone
+                {
+                    Debug.Log("FindNearestEnemy found a candidate!");
+                    RectTransform rt = (RectTransform)transform;
+                    float dist = Vector2.Distance(rt.anchoredPosition, ((RectTransform)(enemy.transform)).anchoredPosition);
+                    if (dist <= minDist)
+                    {
+                        Debug.Log("found a new minEnemy!");
+                        minDist = dist;
+                        minEnemy = enemy;
+                    }
+                }
+            }
+        }
+        return minEnemy;
     }
 
     //uses position to return lane ID, 1-6 clockwise
